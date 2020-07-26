2592bb404fc8646161c576170bb09a2af31379cc
SofianD <doual.sofian@hotmail.com>
2020-07-27 00:56:37 +0200

    create angular material module

---
+++ src/app/shared/ng-material/ng-material.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { MatCardModule } from "@angular/material";
+
+@NgModule({
+  exports: [
+    MatCardModule
+  ]
+})
+export class NgMaterialModule { }
