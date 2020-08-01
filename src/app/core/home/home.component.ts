cea07f69cc5273a0d4c3bdb69426d13446db61d0
SofianD <doual.sofian@hotmail.com>
2020-08-02 00:08:38 +0200

    add carousel 3D

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 import db from "src/assets/db_projects/links-of-skills-img.json";
 
 @Component({
@@ -6,15 +6,36 @@ import db from "src/assets/db_projects/links-of-skills-img.json";
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
 
   linksOfSkillsImg: string[];
 
+  carousel: any; 
+  cellCount = 5;
+  selectedIndex = 0;
+  rotateCarousel = setInterval(
+    () => {
+      let a = document.getElementsByClassName('carousel') as HTMLCollectionOf<HTMLElement>;
+      this.selectedIndex++;
+      let angle = this.selectedIndex / this.cellCount * -360;
+      a[0].style.transform = 'rotateY(' + angle + 'deg)';
+    },
+    4500
+  );
   constructor() {
     this.linksOfSkillsImg = db.links;
+    
   }
-
+  
   ngOnInit() {
+    
   }
 
+  ngAfterViewInit() {
+    this.rotateCarousel;
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.rotateCarousel);
+  }
 }
