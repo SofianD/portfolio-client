8520ec3f8d1b3415dcc4372b668e58110c1c3ac2
Sofian <-v>
2020-08-19 11:40:20 +0200

    add spinner in home component

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
-import db from "src/assets/db_projects/links-of-skills-img.json";
+import db from 'src/assets/db_projects/links-of-skills-img.json';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-home',
@@ -10,28 +11,35 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
 
   linksOfSkillsImg: string[];
 
-  carousel: any; 
+  carousel: any;
   cellCount = 5;
   selectedIndex = 0;
   rotateCarousel = setInterval(
     () => {
+      // tslint:disable-next-line: prefer-const
       let a = document.getElementsByClassName('carousel') as HTMLCollectionOf<HTMLElement>;
       this.selectedIndex++;
-      let angle = this.selectedIndex / this.cellCount * -360;
+      const angle = this.selectedIndex / this.cellCount * -360;
       a[0].style.transform = 'rotateY(' + angle + 'deg)';
     },
     4500
   );
-  constructor() {
+  constructor(
+    private spinner: NgxSpinnerService
+  ) {
     this.linksOfSkillsImg = db.links;
-    
   }
-  
+
   ngOnInit() {
-    
+    this.spinner.show();
+    setTimeout(() => {
+      this.spinner.hide();
+    },
+    700);
   }
 
   ngAfterViewInit() {
+    // tslint:disable-next-line: no-unused-expression
     this.rotateCarousel;
   }
 
