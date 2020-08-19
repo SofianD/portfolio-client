a56894cc4d1afddaebf6f79b99b014981cf7b242
Sofian <-v>
2020-08-19 11:39:57 +0200

    add spinner in projects component

--- src/app/core/projects/projects.component.ts
+++ src/app/core/projects/projects.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import db from "src/assets/db_projects/db.json";
+import db from 'src/assets/db_projects/db.json';
+import { NgxSpinnerService } from 'ngx-spinner';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
@@ -10,18 +11,24 @@ export class ProjectsComponent implements OnInit {
   projects: Array<any>;
 
   shownImgs: any[] = [];
-  selectedImage: string = '';
+  selectedImage = '';
 
-  constructor() {
-    this.projects = db.projects
+  constructor(
+    private spinner: NgxSpinnerService
+  ) {
+    this.projects = db.projects;
   }
   ngOnInit() {
-    
+    this.spinner.show();
+    setTimeout(() => {
+      this.spinner.hide();
+    },
+    400);
   }
 
   openImgCanvas(images: any[]) {
     this.shownImgs = images;
-    this.selectImage(this.shownImgs[0])
+    this.selectImage(this.shownImgs[0]);
   }
 
   closeImgCanvas() {
