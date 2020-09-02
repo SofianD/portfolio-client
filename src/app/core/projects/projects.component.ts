2728127c2994bd3b7b862920b84bbc10335d561d
SofianD <doual.sofian@hotmail.com>
2020-09-02 20:29:58 +0200

    add projects animation

--- src/app/core/projects/projects.component.ts
+++ src/app/core/projects/projects.component.ts
@@ -1,29 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 import db from 'src/assets/db_projects/db.json';
 import { NgxSpinnerService } from 'ngx-spinner';
+
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
-export class ProjectsComponent implements OnInit {
+export class ProjectsComponent implements OnInit, AfterViewInit, OnDestroy {
 
   projects: Array<any>;
 
   shownImgs: any[] = [];
   selectedImage = '';
+  observer: IntersectionObserver;
+  items;
 
   constructor(
     private spinner: NgxSpinnerService
   ) {
     this.projects = db.projects;
   }
+
   ngOnInit() {
-    // this.spinner.show();
-    // setTimeout(() => {
-    //   this.spinner.hide();
-    // },
-    // 400);
+
+  }
+
+  ngAfterViewInit() {
+    this.projectsAnimation();
+  }
+
+  ngOnDestroy() {
+    this.observer.disconnect();
+  }
+
+  projectsAnimation() {
+    this.observer = new IntersectionObserver(
+      function(entries) {
+        entries.forEach((elmnt) => {
+          if (elmnt.intersectionRatio > 0.01) {
+            elmnt.target.classList.remove('not-visible');
+          } else {
+            elmnt.target.classList.add('not-visible');
+          }
+        });
+      },
+      {
+        threshold: [0.01]
+      }
+    );
+
+    this.items = document.querySelectorAll('.pj-container');
+
+    // if (NodeList.prototype.forEach === undefined) {
+    //   NodeList.prototype.forEach = Array.prototype.forEach as any;
+    // }
+
+    this.items.forEach(element => {
+      element.classList.add('not-visble');
+      this.observer.observe(element);
+    });
   }
 
   openImgCanvas(images: any[]) {
